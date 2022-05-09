aaaa08148c9513bfbc399d7728be5563740992f7
Nabeel shaikh <72862916+nabeelshaikh7@users.noreply.github.com>
2022-05-09 18:41:58 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+
+sudo apt install grep sed curl openssl mpv aria2 ffmpeg
+
+sudo curl -sL github.com/pystardust/ani-cli/raw/master/ani-cli -o /usr/local/bin/ani-cli 
+
+sudo chmod +x /usr/local/bin/ani-cli
