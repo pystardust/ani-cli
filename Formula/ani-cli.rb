5c4fed73049e0cf6a26f4d5a84b5d3c9aa4237d2
Anton Melnikov <antonsm21@gmail.com>
2026-07-24 08:14:54 +0200

    chore: formula version bump (#1813)
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/ani-cli.rb
+++ Formula/ani-cli.rb
@@ -1,17 +1,17 @@
 class AniCli < Formula
   desc "Cli tool to browse and play anime"
   homepage "https://github.com/pystardust/ani-cli"
-  url "https://github.com/pystardust/ani-cli/archive/refs/tags/v4.14.tar.gz"
-  sha256 "be40c779605b9b5eb4c09e63a1f460bcc69d73a3ab6e707279548fb8513c96c0"
+  url "https://github.com/pystardust/ani-cli/archive/refs/tags/v4.15.tar.gz"
+  sha256 "7ede3794978dc2eec87475e0ea96449a604a2589e940c1eab6bfbddb8529f973"
   license "GPL-3.0"
   head "https://github.com/pystardust/ani-cli.git", branch: "master"
 
   depends_on "aria2"
+  depends_on "botan"
   depends_on "ffmpeg"
   depends_on "fzf"
   depends_on "grep"
   depends_on "yt-dlp"
-  depends_on "botan"
   depends_on "mpv" => :recommended
 
   def install
